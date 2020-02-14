bd23bd6290086b0adaea68c42c36089f3a86a004
strutton1476 <strutton1476@lebanon.k12.or.us>
2020-02-13 19:23:20 -0800

    fan controller

--- build/scripts/2020-Pi-Vision.bat
+++ build/scripts/2020-Pi-Vision.bat
@@ -63,7 +63,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\2020-Pi-Vision.jar;%APP_HOME%\lib\gson-2.8.5.jar;%APP_HOME%\lib\wpiutil.jar;%APP_HOME%\lib\ntcore.jar;%APP_HOME%\lib\cscore.jar;%APP_HOME%\lib\cameraserver.jar;%APP_HOME%\lib\opencv-347.jar;%APP_HOME%\lib\wpilibj.jar;%APP_HOME%\lib\wpiHal.jar
+set CLASSPATH=%APP_HOME%\lib\2020-Pi-Vision.jar;%APP_HOME%\lib\gson-2.8.5.jar;%APP_HOME%\lib\wpiutil.jar;%APP_HOME%\lib\ntcore.jar;%APP_HOME%\lib\cscore.jar;%APP_HOME%\lib\cameraserver.jar;%APP_HOME%\lib\opencv-347.jar;%APP_HOME%\lib\wpilibj.jar;%APP_HOME%\lib\wpiHal.jar;%APP_HOME%\lib\pi4j-core.jar;%APP_HOME%\lib\pi4j-core-sources.jar;%APP_HOME%\lib\pi4j-core-javadoc.jar
 
 @rem Execute 2020-Pi-Vision
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %2020_PI_VISION_OPTS%  -classpath "%CLASSPATH%" Main %CMD_LINE_ARGS%
---
+++ src/main/java/team1359/FanControl.java
@@ -0,0 +1,5 @@
+package team1359;
+
+public class FanControl{
+    
+}
