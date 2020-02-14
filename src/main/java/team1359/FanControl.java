3a00fb97b931cb7d48f4bc24b18ad9c2ee56520a
strutton1476 <strutton1476@lebanon.k12.or.us>
2020-02-14 15:08:29 -0800

    worked on RPI gpio pins

--- src/main/java/team1359/FanControl.java
+++ src/main/java/team1359/FanControl.java
@@ -1,5 +1,22 @@
 package team1359;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinMode;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
 public class FanControl{
-    
+    public FanControl(){
+        GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Fan Power", PinState.HIGH);
+
+        // set shutdown state for this pin
+        pin.setShutdownOptions(true, PinState.LOW);
+
+        // turn off gpio pin #01
+        pin.low();
+        gpio.shutdown();
+    }
 }
